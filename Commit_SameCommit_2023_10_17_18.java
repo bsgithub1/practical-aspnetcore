60bfa45132130354c52ba31583c1af566fe050be
saurabh.patil <saurabh.pa@browserstack.com>
2023-10-17 18:48:25 +0530

    "Commit :81"

--- Commit_SameCommit_2023_10_17_18.java
+++ Commit_SameCommit_2023_10_17_18.java
@@ -1,10 +1,10 @@
-public class Commit80 {
+public class Commit81 {
 
 	public static void main(String args[]) {
 		try {
 			System.out.println("Screenshot taken for test : ");
 		} catch (Exception e) {
-			System.out.println("Exception 80");
+			System.out.println("Exception 81");
 		}
 	}
 }
