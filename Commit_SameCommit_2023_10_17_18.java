80c187b08f29bb83a77693e076118c8018938515
saurabh.patil <saurabh.pa@browserstack.com>
2023-10-17 18:48:25 +0530

    "Commit :85"

--- Commit_SameCommit_2023_10_17_18.java
+++ Commit_SameCommit_2023_10_17_18.java
@@ -1,10 +1,10 @@
-public class Commit84 {
+public class Commit85 {
 
 	public static void main(String args[]) {
 		try {
 			System.out.println("Screenshot taken for test : ");
 		} catch (Exception e) {
-			System.out.println("Exception 84");
+			System.out.println("Exception 85");
 		}
 	}
 }
