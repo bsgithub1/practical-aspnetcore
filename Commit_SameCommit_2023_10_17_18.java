3ca9c30a2b84a39c7617371bc1dd0364dba62f6b
saurabh.patil <saurabh.pa@browserstack.com>
2023-10-17 18:48:22 +0530

    "Commit :48"

--- Commit_SameCommit_2023_10_17_18.java
+++ Commit_SameCommit_2023_10_17_18.java
@@ -1,10 +1,10 @@
-public class Commit47 {
+public class Commit48 {
 
 	public static void main(String args[]) {
 		try {
 			System.out.println("Screenshot taken for test : ");
 		} catch (Exception e) {
-			System.out.println("Exception 47");
+			System.out.println("Exception 48");
 		}
 	}
 }
