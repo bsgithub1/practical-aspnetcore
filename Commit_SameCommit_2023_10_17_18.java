ac1d53ededbd0fed2b61be91dc1b7f71fb610259
saurabh.patil <saurabh.pa@browserstack.com>
2023-10-17 18:48:23 +0530

    "Commit :65"

--- Commit_SameCommit_2023_10_17_18.java
+++ Commit_SameCommit_2023_10_17_18.java
@@ -1,10 +1,10 @@
-public class Commit64 {
+public class Commit65 {
 
 	public static void main(String args[]) {
 		try {
 			System.out.println("Screenshot taken for test : ");
 		} catch (Exception e) {
-			System.out.println("Exception 64");
+			System.out.println("Exception 65");
 		}
 	}
 }
