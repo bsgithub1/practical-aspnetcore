26a1ae9c15dac80b6aad2f3ef64c86fed8363d5e
saurabh.patil <saurabh.pa@browserstack.com>
2023-10-17 18:28:57 +0530

    "Commit :4"

--- Commit_SameCommit_2023_10_17_18.java
+++ Commit_SameCommit_2023_10_17_18.java
@@ -1,10 +1,10 @@
-public class Commit3 {
+public class Commit4 {
 
 	public static void main(String args[]) {
 		try {
 			System.out.println("Screenshot taken for test : ");
 		} catch (Exception e) {
-			System.out.println("Exception 3");
+			System.out.println("Exception 4");
 		}
 	}
 }
