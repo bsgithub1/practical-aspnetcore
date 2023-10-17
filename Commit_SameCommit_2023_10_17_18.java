a1ddcb5675206e8924a29866eb7af1d31eeaa015
saurabh.patil <saurabh.pa@browserstack.com>
2023-10-17 18:48:20 +0530

    "Commit :29"

--- Commit_SameCommit_2023_10_17_18.java
+++ Commit_SameCommit_2023_10_17_18.java
@@ -1,10 +1,10 @@
-public class Commit28 {
+public class Commit29 {
 
 	public static void main(String args[]) {
 		try {
 			System.out.println("Screenshot taken for test : ");
 		} catch (Exception e) {
-			System.out.println("Exception 28");
+			System.out.println("Exception 29");
 		}
 	}
 }
